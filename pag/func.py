0135f9b1dd090155333dd9c02c385dd3724983a7
Frenz86 <frenzy86@gmail.com>
2025-04-23 19:36:32 +0200

    new

--- pag/func.py
+++ pag/func.py
@@ -621,7 +621,7 @@ class Funz:
             go.Bar(
                 x=fase_levels.index,
                 y=fase_levels.values,
-                text=percentages.str.replace('%', '').astype(float) * 10, # Format percentages
+                text=percentages.apply(lambda x: float(f"{x:.1f}".replace(',', '.')) * 10), # Format percentages
                 textposition='auto',
                 marker=dict(color=self.colors_red[:len(fase_levels)])
             ),
