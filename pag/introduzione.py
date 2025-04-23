c4303194ffff3be1d27072adc75c50e1f8bca13e
Frenz86 <frenzy86@gmail.com>
2025-04-23 21:26:14 +0200

    new

--- pag/introduzione.py
+++ pag/introduzione.py
@@ -7,16 +7,16 @@ def main():
    st.image("./img/ecosistemi.jpg")
 
    text= """
-         ## Rapporto Finale dell'Osservatorio sulla Maturità Digitale 2024  
+## Rapporto Finale dell'Osservatorio sulla Maturità Digitale 2024  
 
-         ### Prefazione
-         Il presente rapporto è stato realizzato grazie al contributo essenziale della Fondazione Cassa dei
-         Risparmi di Forlì, che ha reso possibile la ricerca e la messa a disposizione dei risultati all'intera
-         comunità imprenditoriale e istituzionale del territorio. Attraverso questo sostegno, la Fondazione
-         conferma il proprio impegno nel promuovere lo sviluppo economico e l'innovazione locale,
-         contribuendo a costruire un ecosistema digitale territoriale più competitivo e resiliente.
-         L'Osservatorio sulla Maturità Digitale rappresenta uno strumento strategico per comprendere lo
-         stato attuale della trasformazione digitale e orientare azioni concrete per il futuro.
+### Prefazione
+Il presente rapporto è stato realizzato grazie al contributo essenziale della Fondazione Cassa dei
+Risparmi di Forlì, che ha reso possibile la ricerca e la messa a disposizione dei risultati all'intera
+comunità imprenditoriale e istituzionale del territorio. Attraverso questo sostegno, la Fondazione
+conferma il proprio impegno nel promuovere lo sviluppo economico e l'innovazione locale,
+contribuendo a costruire un ecosistema digitale territoriale più competitivo e resiliente.
+L'Osservatorio sulla Maturità Digitale rappresenta uno strumento strategico per comprendere lo
+stato attuale della trasformazione digitale e orientare azioni concrete per il futuro.
 
 # Indice
 
